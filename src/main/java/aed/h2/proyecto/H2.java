8073d21036f3b901a2c36270c32d537c4b0d5a35
David <davidcm097@gmail.com>
2021-01-30 19:53:15 +0000

    Initial commit

---
+++ src/main/java/aed/h2/proyecto/H2.java
@@ -0,0 +1,10 @@
+package aed.h2.proyecto;
+
+public class H2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/main/java/aed/h2/proyecto/Main.java
@@ -0,0 +1,10 @@
+package aed.h2.proyecto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
