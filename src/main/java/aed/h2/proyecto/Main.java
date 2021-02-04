35d631c5a9c45560a7d7c65ab2f14ba462786f4d
David <davidcm097@gmail.com>
2021-02-04 18:46:01 +0000

    Tabla Peliculas sentencias

--- src/main/java/aed/h2/proyecto/Main.java
+++ src/main/java/aed/h2/proyecto/Main.java
@@ -3,13 +3,15 @@ package aed.h2.proyecto;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		try {
 			Coneccion conexion =new Coneccion();
-			conexion.probar();
+			//conexion.probar();
 			//conexion.inser();
+			//conexion.mod();
+			conexion.borrar();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
