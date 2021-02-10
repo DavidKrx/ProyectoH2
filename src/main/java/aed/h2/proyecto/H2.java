bd0a03afcd5b890a55d146e5ba6a808bb3d224bc
David <davidcm097@gmail.com>
2021-02-10 20:26:31 +0000

    Acabado

--- src/main/java/aed/h2/proyecto/H2.java
+++ src/main/java/aed/h2/proyecto/H2.java
@@ -1,11 +1,17 @@
 package aed.h2.proyecto;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class H2 {
@@ -22,7 +28,7 @@ Scanner sc=new Scanner(System.in);
 				conect=DriverManager.getConnection("jdbc:h2:C:\\Users\\david\\eclipse-workspace\\ProyectoH2\\lib\\databaseH2","sa","");
 				//conect=DriverManager.getConnection("jdbc:h2:C:\\Users\\Usuario\\eclipse-workspace2\\ProyectoH2\\lib\\databaseH2","sa","");
 				st=conect.createStatement();
-				System.out.println("Conectó");
+				//System.out.println("Conectó");
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -30,20 +36,20 @@ Scanner sc=new Scanner(System.in);
 		
 	}
 	
-	void probar() {
+	void visualizarSala() {
 		try {
 			
 			rst=st.executeQuery("SELECT * FROM CINE.SALA");
 			while(rst.next()) {
 				System.out.println("ID: "+rst.getInt("ID")+"\t"+"IdPelicula: "+rst.getInt("IDPelicula")+"\t"+"Hora: "+rst.getString("Hora")
-				+"\t"+"3D:"+rst.getBoolean("TRESD")+ " 3D");
+				+"\t"+"3D:"+rst.getBoolean("TRESD"));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	void inser() {
+	void inserSala() {
 		try {
 			//rst=st.prepare(,5);
 			PreparedStatement a= conect.prepareStatement("INSERT INTO CINE.SALA (IDPelicula, Hora, TRESD)VALUES('2','20:20:20','0');");
@@ -55,24 +61,33 @@ Scanner sc=new Scanner(System.in);
 		}
 	}
 	
-	void mod() {
-		probar();
+	@SuppressWarnings("deprecation")
+	void modSala(){
+		visualizarSala();
 		System.out.println("Id a modificar");
 		int a=sc.nextInt();
 		System.out.println("IdPelicula a modificar");
 		int b=sc.nextInt();
-		System.out.println("Hora a modificar");
-		String c=sc.nextLine();
+		//System.out.println("Hora a modificar");
+		//String c=sc.nextLine();
+		
 		System.out.println("3D a modificar");
-		String d=sc.nextLine();
+		boolean d=sc.nextBoolean();
 
 		try {
 			//rst=st.prepare(,5);
-			
-			PreparedStatement ax= conect.prepareStatement("UPDATE CINE.PELICULAS SET IDPelicula=?, Hora=?, TRESD=? where ID="+a);
+//			String p=c.substring(0, 2);
+//			String l=c.substring(4, 5);
+//			String m=c.substring(7, 8);
+//			int u=Integer.parseInt(p); 
+//			int i=Integer.parseInt(l);
+//			int o=Integer.parseInt(m);
+			Time j;
+			j=new Time(20,20,20);
+			PreparedStatement ax= conect.prepareStatement("UPDATE CINE.SALA SET IDPelicula=?, Hora=?, TRESD=? where ID="+a);
 			ax.setInt(1, b);
-			ax.setString(2, c);
-			ax.setString(3, d);
+			ax.setTime(2, j);
+			ax.setBoolean(3, d);
 
 			ax.executeUpdate();
 			System.out.println("se ha modificado"+"\n");
@@ -84,14 +99,14 @@ Scanner sc=new Scanner(System.in);
 		try {
 			
 			Thread.sleep(2000);
-			probar();
+			visualizarSala();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
-	void borrar() {
+	void borrarSala() {
 		System.out.println("Id a borrar");
 		String a=sc.nextLine();
 		try {
@@ -108,11 +123,102 @@ Scanner sc=new Scanner(System.in);
 		try {
 			
 			Thread.sleep(2000);
-			probar();
+			visualizarSala();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	void visualizarPeliculas() {
+		try {
+			
+			rst=st.executeQuery("SELECT * FROM CINE.PELICULAS");
+			while(rst.next()) {
+				System.out.println("IDpeli: "+rst.getInt("IDPelicula")+"\t"+"Titulo: "+rst.getString("Titulo")+"\t"+"Genero: "+rst.getString("Genero")
+				+"\t"+"Rango de edad:"+rst.getInt("RangoEdad")+ " años");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	void inserPeliculas() {
+		try {
+			//rst=st.prepare(,5);
+			PreparedStatement a= conect.prepareStatement("INSERT INTO CINE.PELICULAS (Titulo, Genero, Rangoedad)VALUES('Cielo2','Misterio2','18');");
+			a.executeUpdate();
+			System.out.println("se insertó");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	void modPeliculas() {
+		visualizarPeliculas();
+		System.out.println("Id a modificar");
+		String a=sc.nextLine();
+		System.out.println("Titulo a modificar");
+		String b=sc.nextLine();
+		System.out.println("Genero a modificar");
+		String c=sc.nextLine();
+		System.out.println("Rangoedad a modificar");
+		String d=sc.nextLine();
 
+		try {
+			//rst=st.prepare(,5);
+			
+			PreparedStatement ax= conect.prepareStatement("UPDATE CINE.PELICULAS SET Titulo=?, Genero=?, Rangoedad=? where IDPelicula="+a);
+			ax.setString(1, b);
+			ax.setString(2, c);
+			ax.setString(3, d);
+
+			ax.executeUpdate();
+			System.out.println("se ha modificado"+"\n");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			
+			Thread.sleep(2000);
+			visualizarPeliculas();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	void borrarPeliculas() {
+		System.out.println("Id a borrar");
+		String a=sc.nextLine();
+		try {
+			
+			PreparedStatement ax= conect.prepareStatement("DELETE FROM CINE.PELICULAS Where IDPelicula=?");
+			ax.setString(1, a);
+			ax.executeUpdate();
+			System.out.println("se borro");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			Thread.sleep(2000);
+			visualizarPeliculas();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	void cerrar() {
+		try {
+			conect.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/aed/h2/proyecto/Main.java
+++ src/main/java/aed/h2/proyecto/Main.java
@@ -1,24 +1,73 @@
 package aed.h2.proyecto;
 
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class Main {
-
+	static Scanner sc = new Scanner(System.in);
+	static boolean salirInicio = false;
 	public static void main(String[] args) {
-
-		try {
-			//Coneccion conexion =new Coneccion();
-			//conexion.probar();
-			//conexion.inser();
-			//conexion.mod();
-			//conexion.borrar();
-			H2 salsa=new H2();
-			//salsa.probar();
-			//salsa.inser();
-			//salsa.mod();
-			salsa.borrar();
-		} catch (ClassNotFoundException e) {
-			
-			e.printStackTrace();
-		}
 		
-	}
-}
+		while (salirInicio == false) {
+			menuInicial();
+		}if(salirInicio == true) {
+		}	}
+		
+		
+			
+	
+			
+	private static void menuInicial() {
+		H2 base = null;
+		
+	try {
+	base = new H2();	
+	System.out.println("\n 1: Visualizar peliculas \n 2: Visualizar salas \n 3: Insertar peliculas \n 4: Insertar Salas \n 5: Borrar peliculas \n 6: Borrar salas \n 7: Modificar peliculas \n 8: Modificar sala \n 9: Salir \n Decide que quiere realizar:");	
+				String b=sc.nextLine();
+			
+				switch (b) {
+				
+				case "1":
+					base.visualizarPeliculas();
+					
+					break;
+				case "2":
+					base.visualizarSala();
+				
+					break;
+				case "3":
+					base.inserPeliculas();
+				
+					break;
+				case "4":
+					base.inserSala();
+				
+					break;
+				case "5":
+					base.borrarPeliculas();
+				
+					break;
+				case "6":
+					base.borrarSala();
+				
+					break;
+				case "7":
+					base.modPeliculas();
+				
+					break;
+				case "8":
+						base.modSala();
+				
+					break;
+				case "9":
+					salirInicio=true;
+					base.cerrar();
+					System.exit(0);
+					break;
+				default:
+					base.cerrar();
+					break;
+				}} catch (ClassNotFoundException e) {
+					e.printStackTrace();		
+	}}}
